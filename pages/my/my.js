17d383bd6efd25409c611a56dfde1404286211a2
majing <majing0711@foxmail.com>
2018-12-11 22:56:17 +0800

    个人中心页面完成

--- pages/my/my.js
+++ pages/my/my.js
@@ -1,3 +1,5 @@
+
+const app = getApp()
 // pages/my/my.js
 Page({
 
@@ -5,62 +7,90 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    userInfo: {},
+    hasPhoneNumber:false,
+    canIUse: wx.canIUse('button.open-type.getPhoneNumber'),
+    userName:'张三',
+    compony:'百度',
+    listData:[
+      {"code":"01","text":"text1","type":"type1"},
+      {"code":"02","text":"text2","type":"type2"},
+      {"code":"03","text":"text3","type":"type3"},
+      {"code":"04","text":"text4","type":"type4"},
+      {"code":"05","text":"text5","type":"type5"},
+      {"code":"06","text":"text6","type":"type6"},
+      {"code":"07","text":"text7","type":"type7"}
+    ]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    var that = this;
+    wx.showLoading({
+      title: '加载中',
+    })
+    setTimeout(function () {
+      var userInfo = app.globalData.userInfo 
+      that.setData({
+        userInfo: userInfo
+      })
+      wx.hideLoading()
+    }, 1000)
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
-  }
+
+  },
+  bindKeyInput(e) {
+    this.setData({
+      [e.target.dataset.name]: e.detail.value
+    })
+  },
 })
